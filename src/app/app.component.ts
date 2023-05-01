c6a74be16e3d8ee78dc1fc71d132903ee0a6c5ce
Billy Nowosielski <billyn@bu.edu>
2023-05-01 06:08:41 -0400

    finished hw6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface MyData {
+  event: string;
+  description: string;
+  effective: string;
+  expires: string;
+  status: string;
+  // ... any other properties your data has
+}
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +19,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'untitled1';
+  myData: MyData[] = [];
+  constructor(private http: HttpClient) {}
+
+
+  fetchData() {
+    this.http.get('/assets/mock-data.json').subscribe((data: any) => {
+      this.myData = data.map((feature: any) => ({
+        event: feature.properties.event,
+        description: feature.properties.description,
+        effective: feature.properties.effective,
+        expires: feature.properties.expires,
+        status: feature.properties.status,
+        // ... any other properties your data has
+      }));
+    });
+  }
 }
